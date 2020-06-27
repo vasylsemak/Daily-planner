a2887a0861de9e1ea5f13b1cab66536b73f2fcf9
Vasyl Semak <vasylsmk1401@gmail.com>
2020-06-27 13:41:50 -0400

    create: get/post listItems; index.ejs file

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+const items = ['Eat', 'Play Piano', 'Study'];
+const workItems = [];
+
+
+app.get('/', (req, res) => {
+  const today = date.getDate();
+
+  res.render('index', {
+    listTitle: today,
+    newListItems: items,
+  });
 });
+
+
+app.post('/', (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+
+  res.redirect('/');
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000.'));
