01e60cf5b02ab44ca443fa9567da2d86f6d75ab4
Vasyl Semak <vasylsmk1401@gmail.com>
2020-06-27 11:13:59 -0400

    create getDay and getDate

---
+++ date.js
@@ -0,0 +1,15 @@
+const date = new Date();
+
+exports.getDate = () => {
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+  };
+  return date.toLocaleDateString('en-US', options);
+};
+
+exports.getDay = () => {
+  const options = { weekday: 'long' };
+  return date.toLocaleDateString('en-US', options);
+};
