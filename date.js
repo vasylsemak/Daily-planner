a5e9a9b6b7b845bac78c82c3a2692fa36565eb04
Vasyl Semak <vasylsmk1401@gmail.com>
2020-06-28 20:23:38 -0400

    create custom list basedon user url input

--- date.js
+++ date.js
@@ -1,15 +1,15 @@
-// const date = new Date();
+const date = new Date();
 
-// exports.getDate = () => {
-//   const options = {
-//     weekday: 'long',
-//     month: 'long',
-//     day: 'numeric',
-//   };
-//   return date.toLocaleDateString('en-US', options);
-// };
+exports.getDate = () => {
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+  };
+  return date.toLocaleDateString('en-US', options);
+};
 
-// exports.getDay = () => {
-//   const options = { weekday: 'long' };
-//   return date.toLocaleDateString('en-US', options);
-// };
+exports.getDay = () => {
+  const options = { weekday: 'long' };
+  return date.toLocaleDateString('en-US', options);
+};
